76a814e194b2b82f8baed54caf2deeffe9d2a60c
romandogadin <48025741+romandogadin@users.noreply.github.com>
2023-08-11 16:21:13 -0400

    exercises

--- 02/2.05.0/js/main.js
+++ 02/2.05.0/js/main.js
@@ -4,6 +4,40 @@
 *    2.5 - Activity: Adding SVGs to the screen
 */
 
-const svg = d3.select('#chart-area').attr("width",500).attr("height",400)
+const svg = d3.select('#chart-area').append("svg")
+    .attr("style", "outline: thin solid black;")
+    .attr("width",500)
+    .attr("height",400)
 
-svg.append("line").attr("x1",5).attr("y1",5).attr("x2",25).attr("y2",5).attr("stroke","green").attr("stroke-width",5).attr("fill","green")
+// line
+svg.append("line")
+    .attr("x1",5)
+    .attr("y1",20)
+    .attr("x2",250)
+    .attr("y2",20)
+    .attr("stroke","green")
+    .attr("stroke-width",5)
+    .attr("fill","green")
+
+
+// circle
+svg.append("circle")
+    .attr("cx",50)
+    .attr("cy",70)
+    .attr("r",30)
+
+// rectangle
+svg.append("rect")
+    .attr("x",150)
+    .attr("y",150)
+    .attr("width",200)
+    .attr("height",100)    
+    .attr("fill","blue")
+
+// ellipse
+svg.append("ellipse")
+    .attr("cx",350)
+    .attr("cy",300)
+    .attr("rx",100)
+    .attr("ry",30)
+    .attr("fill","purple")    
--- 02/2.08.0/js/main.js
+++ 02/2.08.0/js/main.js
@@ -3,3 +3,25 @@
 *    Mastering Data Visualization with D3.js
 *    2.8 - Activity: Your first visualization!
 */
+
+const svg = d3.select("#chart-area").append("svg")
+    //.attr("style", "outline: thin solid black;")
+    .attr("width", 500)
+    .attr("height", 500)
+
+d3.json("data/buildings.json").then(
+        data => {data.forEach(
+            d => {d.height = Number(d.height)}
+        )
+        console.log(data)
+
+        const rectangles = svg.selectAll("rect")
+            .data(data)
+
+        rectangles.enter().append("rect")
+            .attr("x", (d,i) => i*100)
+            .attr("y", (d,i) => 500-d.height)
+            .attr("width", 70)
+            .attr("height", (d) => d.height)
+            .attr("fill", "green")
+    })
